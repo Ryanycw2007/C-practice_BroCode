8a6311ac9d39a187e4d91e5f9b26dbf83cc062d2
Ryan <ryanycw.dev@gmail.com>
2026-05-22 12:08:08 +0800

    add BreakContinue.c to demonstrate break and continue statements in a loop

---
+++ BreakContinue.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+//break = Break out of a loop (stop)
+//continue = skip current cycle of a loop(skip)
+
+int main()
+{
+    for(int i = 1; i <= 10; i++)
+    {
+        if(i == 4)
+        {
+            continue;
+        }
+        
+        
+        printf("%d\n", i);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
