71c06dcf627154896fc8ea5c2c52d7e48bccb7ba
Ryan <ryanycw.dev@gmail.com>
2026-05-22 11:41:15 +0800

    add spam.c to demonstrate MessageBox functionality in a loop

---
+++ spam.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+    for(int i = 0; i<20; i++)
+    {
+        MessageBox(NULL, "I WILL DESTROY YOUR COMPUTER", "Message From USER_&*fh^^0", MB_RETRYCANCEL | MB_ICONINFORMATION);
+    }
+    return 0;
+}
